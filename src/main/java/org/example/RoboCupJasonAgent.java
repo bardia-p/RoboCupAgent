57bf40bfbb6595ea2b6907222759be8e6ed8b83d
Bardia Parmoun <bardiaparmoun@gmail.com>
2024-11-21 00:15:24 -0500

    - Added jason as a dependency. - Added an empty agent class that imports jason methods.

---
+++ src/main/java/org/example/RoboCupJasonAgent.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import jason.architecture.AgArch;
+import jason.asSemantics.ActionExec;
+import jason.asSemantics.Agent;
+import jason.asSemantics.TransitionSystem;
+import jason.asSyntax.Literal;
+import jason.infra.local.RunLocalMAS;
+
+/**
+ * Example of an agent that only uses Jason BDI engine. It runs without all
+ * Jason IDE stuff. (see Jason FAQ for more information about this example)
+ *
+ * The class must extend AgArch class to be used by the Jason engine.
+ */
+public class RoboCupJasonAgent extends AgArch {
+}
